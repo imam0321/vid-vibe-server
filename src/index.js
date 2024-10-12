158e3961c802e8ca01163d823a9cedd4a04a6ef8
imam0321 <imam.hossain0321@gmail.com>
2024-10-13 01:33:16 +0600

    added middlewares and connect the db

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("hello");
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env"
+})
+
+const PORT = process.env.PORT || 5001;
+
+
+connectDB()
+.then(() => {
+  app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`);
+  })
+})
+.catch((err) => {
+  console.log("MongoBD connection error ", err);
+})
