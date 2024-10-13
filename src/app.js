6ae2e82c084cd7b178d66bbbd7309d975bc621ef
imam0321 <imam.hossain0321@gmail.com>
2024-10-14 00:08:45 +0600

    added all model schema

--- src/app.js
+++ src/app.js
@@ -2,15 +2,21 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-
+ 
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
   Credentials: true
 }));
 
+// Common Middlewares
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"))
+
+// import routes 
+import heathCheckRouter from "./routes/healthCheck.routes.js"
+
+app.use("/api/v1/healthCheck", heathCheckRouter)
 
 
 export {app};
